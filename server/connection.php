af7cc9e028b98dc7df2e7360218dfe97e9598a75
Dushan Senadheera <dcsenadheera777@gmail.com>
2023-05-29 18:18:16 +0530

    modified db connection

--- server/connection.php
+++ server/connection.php
@@ -1,9 +1,9 @@
 <?php
 
-$servername = "localhost";
-   $username = "esp32";
-   $password = "microcontrollerslab@123";
-   $database_name = "database_ESP32";
+$servername = "kfgk8u2ogtoylkq9.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
+   $username = "kih0ldprjrs8vcm7";
+   $password = "wxfpj7z0q9gblr3z";
+   $database_name = "p384g3ggmy9dzqzv";
 
    $connection = new mysqli($servername, $username, $password, $database_name);
 
@@ -12,3 +12,5 @@ $servername = "localhost";
    }
    
 ?>
+
+mysql://kih0ldprjrs8vcm7:wxfpj7z0q9gblr3z@kfgk8u2ogtoylkq9.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/p384g3ggmy9dzqzv
